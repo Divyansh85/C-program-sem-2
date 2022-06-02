fa54450c0b558d5367eaf88ba9c5813a6d636854
Divyansh85 <91238471+Divyansh85@users.noreply.github.com>
2022-06-02 22:39:43 +0530

    Create 1_sum_2_sum.c

---
+++ basic programs/1_sum_2_sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Sum of two numbers is: %d",a+b);
+    return 0;
+}
