28098cc0bd358b98a784283d53512721407eefa6
Divyansh85 <91238471+Divyansh85@users.noreply.github.com>
2022-06-02 22:48:27 +0530

    Create pr_01_day_of_week.c

---
+++ switch_case/pr_01_day_of_week.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter day: ");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+        printf("Sunday");
+            break;
+        case 2:
+        printf("Monday");
+            break;
+        case 3:
+        printf("Tuesday");
+            break;
+        case 4:
+        printf("Wednesday");
+            break;
+        case 5:
+        printf("Thursday");
+            break;
+        case 6:
+        printf("Friday");
+            break;
+        case 7:
+        printf("Saturday");
+            break;
+    }
+
+}
